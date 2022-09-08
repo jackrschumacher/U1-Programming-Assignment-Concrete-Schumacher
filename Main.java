bab94e585d03a7392725dd603e62b45803f1536b
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-08 16:38:43 +0000

    Set up scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+  Scanner input = new Scanner(System.in);
+
+  
+  
+
+    
   }
 }
