52c7439fb2cb6ab3ce584cc7b80b41d7eb886abc
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-08 16:50:54 +0000

    Fix bug

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Main {
   public static void main(String[] args) {
 
   Scanner input = new Scanner(System.in);
-  system.out.println("What is your name?:");
+  System.out.println("What is your name?:");
   String username = input.next();
     
   
