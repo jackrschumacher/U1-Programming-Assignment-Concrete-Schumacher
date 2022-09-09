15991af4246273b5ecc0cc7f6f238ba1a03ac692
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-09 16:33:58 +0000

    Add print out from results.

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ class Main {
   Scanner input = new Scanner(System.in);
   System.out.println("What is your name?:");
   String username = input.next();
+  System.out.println("Ok! Your name is" +  username);
+    
     
   
 
