9745fcb4befd22b69fc2ae16afe2aa636680dc9c
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-12 18:52:51 +0000

    Delete poolWidth

--- Main.java
+++ Main.java
@@ -17,17 +17,7 @@ class Main {
   System.out.println("What is the width of your pool (in feet)");
   double poolWidth = input.nextDouble();
   System.out.println("Ok! The length of your pool is:" + poolWidth + " feet.");
-  double poolArea = poolLength * poolWidth / 3; //Multiply pool length by pool width and then divide by 3 in order to convert to yards
-  double poolPlusConcrete = poolLength+12  * poolWidth+12 ;
-    
-
-  double concreteArea = poolPlusConcrete - poolArea;
   
-  double pricePerYard = 419.95; //Set up constant PPY variable
-  double concreteCost = pricePerYard * concreteArea; 
-  double tax = 0.055 * concreteCost;
-  double totalCost = 1200+ concreteCost + tax;
-  System.out.println("Your bill is:" + totalCost);
     
   
 
