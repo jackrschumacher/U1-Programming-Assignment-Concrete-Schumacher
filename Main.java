62b680861188566312bcbd0a30de5a954bb50dc5
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-12 15:59:46 +0000

    fix username next line

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ class Main {
 
   Scanner input = new Scanner(System.in);
   System.out.println("What is your name?:");
-  String username = input.next();
+  String username = input.nextLine();
   System.out.println("Ok! Your name is " +  username);
   System.out.println("What is your adress?");
   String adress = input.nextLine();
