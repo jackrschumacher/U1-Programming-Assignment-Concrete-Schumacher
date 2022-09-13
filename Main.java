dba55de18a676175e2e640d2cbc568a7e7697bcf
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-09-13 15:22:43 +0000

    Comments

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ class Main {
   Scanner input = new Scanner(System.in);
   System.out.println("What is your name?:"); //Input, name, address and the dimensions of your pool 
   String username = input.nextLine();
-  System.out.println("Ok! Your name is " +  username);
+  System.out.println("Ok! Your name is " +  username); //Print out what was inputed from the user
   System.out.println("What is your address?");
   String address = input.nextLine();
   System.out.println("Ok! Your address is "+ address);
@@ -19,11 +19,10 @@ class Main {
   System.out.println("Ok! The length of your pool is:" + poolWidth + " feet.");
   double poolArea = poolLength * poolWidth; //Calculate the total pool area by Multiplying length * width
   poolLength = poolLength + 12;
-  poolWidth = poolWidth + 12; //add twelve because add 6 to each side
+  poolWidth = poolWidth + 12; //add twelve because you are adding 6 in length and width on each side
   double concreteArea = poolWidth * poolWidth;
   concreteArea = concreteArea - poolArea;
   concreteArea = concreteArea * 0.5;
-  
   double pricePerYard = 419.95;
   concreteArea = concreteArea / 27; //Divide by 3 in order to convert to yards and then divide by 9, which gets you 27
   double bill = 1200 + (0.055*concreteArea*pricePerYard) + (pricePerYard * concreteArea);
